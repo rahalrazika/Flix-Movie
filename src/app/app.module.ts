67603a5d1b8515f65402317b5bcdcb6593442fef
rahalrazika <rahalraika@gmail.com>
2023-09-26 18:44:42 +0100

    Setup MUI for the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,23 @@ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { RegistrationComponent } from './registration/registration.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [AppComponent, RegistrationComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    MatToolbarModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    ReactiveFormsModule,
     provideFirebaseApp (() => initializeApp(environment.firebaseConfig)),
     provideAuth( () => getAuth())
   ],
