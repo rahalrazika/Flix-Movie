28ad08801be93ff1b1a99c2f4beaebb8562ec7aa
rahalrazika <rahalraika@gmail.com>
2023-09-26 17:42:42 +0100

    setting up firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, RegistrationComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp (() => initializeApp(environment.firebaseConfig)),
+    provideAuth( () => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyD9zzihLumu6dKnIexaX6-BRZoLpt3ZO0E",
+        authDomain: "flix-movie-cdf13.firebaseapp.com",
+        projectId: "flix-movie-cdf13",
+        storageBucket: "flix-movie-cdf13.appspot.com",
+        messagingSenderId: "705072689344",
+        appId: "1:705072689344:web:c0c9c26d07723eea157d25",
+        measurementId: "G-XH9W5R6XGP"
+    }
+};
